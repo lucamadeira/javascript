e8ea7bf11ddda327b521397f8126eb2aa599fc58
luca madeira valentini <lucamadeiravalentini@gmail.com>
2025-06-08 14:07:36 +0200

    devo finire di fare

---
+++ exercicios de javascript/js010.js
@@ -0,0 +1,8 @@
+let  pais = `EUROPA`
+console.log(`PAIS ${pais} `)
+if (pais != `BRASIL`){
+    console.log(` SEI EUROPEO`)
+}
+else {
+    console.log(`TU NO SEI EUROPEO`)
+}
--- exercicios de javascript/js09.js
+++ exercicios de javascript/js09.js
@@ -1,1 +1,7 @@
-console.log(`ciao funzina`) 
+let velocidade = 50
+console.log(`a velocidade do seu carro e ${velocidade}km/h`)
+if (velocidade > 60){ // codiçao simples por que e true p false 
+    console.log(`voce passou a velocidad permitida multado`)
+}
+console.log(`dirigia sempre com cuidado`)
+// il if permete  di se caso la macchina ultrapasse la velicida permitida viene multado 
