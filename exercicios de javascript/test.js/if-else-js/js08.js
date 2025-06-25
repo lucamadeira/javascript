0b9b2c45b28f4fb47cc3ff7b74da13b6cba8722d
Wanderson Madeira <wandersonmadeira@Host-023.homenet.telecomitalia.it>
2025-06-25 17:36:31 +0200

    aggiornamneto della interface della pagina o merso altre elementi

--- exercicios de javascript/test.js/if-else-js/js08.js
+++ exercicios de javascript/test.js/if-else-js/js08.js
@@ -1,11 +1,35 @@
+// let tieneAutorizacion = false;
+// let edad = 22;
+// if (tieneAutorizacion === true || edad > 25)
+// {
+//     console.log("bienvenido ala pagina de adm")
+
+// } else {
+//     console.log("no estas autorizado a entrar ")
+// } 
+
+// let x = 2;
+
+// if (x > 5) {
+//   console.log("x is greater than 5");
+//   x = x - 8;
+// } else {
+//   console.log("x is 5 or less");
+//   x = x + 8;
+// }
+
+// console.log("Final x:", x);
 
 
-let numero = [ "22", "44", "66", "88" ];
-for(let i = 0; i < numero.length; ++i){
-	console.log(numero[i]) 
 
-    if (numero[i] ==  66){
-        break
-    }
+// let numero = [ "22", "44", "66", "88" ];
+// for(let i = 0; i < numero.length; ++i){
+// 	console.log(numero[i]) 
+
+//     if (numero[i] ==  66){
+//         break
+//     }
        
-}
+// }
+for (let i = 22; i <= 50; i ) { console.log("OMG!!! I am "+i+" years old!!!"); }
+
